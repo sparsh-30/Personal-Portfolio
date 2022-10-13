5df92c09e990749679e8354c312f3ca3b8d402bc
Sparsh Gupta <gsparsh04@gmail.com>
2022-10-13 19:10:28 +0530

    Navbar component created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import sanity from './sanity'
+import Navbar from './Components/Navbar';
 
 function App() {
+
+  // useEffect(()=>{
+  //   sanity.fetch(`*[_type=="details"]{
+  //     name,email
+  //   }`).then((data)=>{console.log(data)});
+  // })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+// importing icons
+import { AiFillHome } from "react-icons/ai";
+import { FaUser } from "react-icons/fa";
+import { RiCodeSSlashLine } from "react-icons/ri";
+import { BsFillBriefcaseFill } from "react-icons/bs";
+import { RiContactsBook2Fill } from "react-icons/ri";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='flex fixed w-screen px-12 py-3 flex-wrap shadow-xl items-center justify-between text-l mx-auto lg:justify-evenly lg:py-2'>
+            <a className='text-3xl lg:text-xl' href="/">Sparsh</a>
+            <a className='text-xl' href="/"><BsFillMoonStarsFill /></a>
+        </div>
+        <div className='flex justify-center'>
+            <ul className='flex text-2xl px-8 w-3/4 backdrop-blur-sm bg-[#98A8F8] py-6 rounded-full justify-between fixed bottom-5 items-center lg:w-1/4 lg:text-md lg:py-4'>
+                <li><a href="/"><AiFillHome /></a></li>
+                <li><a href="/"><FaUser /></a></li>
+                <li><a href="/"><RiCodeSSlashLine /></a></li>
+                <li><a href="/"><BsFillBriefcaseFill /></a></li>
+                <li><a href="/"><RiContactsBook2Fill /></a></li>
+            </ul>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
